c1f9cf93480f52b0a7cafa2734e26b2efc2e1d3b
Ronny Alexander Yungan Guerra <ron.n.y25@hotmail.com>
2020-02-04 14:51:13 +0100

    the project is done

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Leanne Graham' ,
+        username: 'Bret', 
+        email: 'Sincere@april.biz'
+    },
+    {
+        id: 2,
+        name: 'Ronny Yungan' ,
+        username: 'Bret', 
+        email: 'Sincere@april.biz'
+    },
+    {
+        id: 3,
+        name: 'Ruben Armando' ,
+        username: 'Bret', 
+        email: 'Sincere@april.biz'
+    }, 
+    {
+        id: 4,
+        name: 'Elizabeth Guerra' ,
+        username: 'Bret', 
+        email: 'Sincere@april.biz'
+    }, 
+    {
+        id: 5,
+        name: 'Alberto Machin' ,
+        username: 'Bret', 
+        email: 'Sincere@april.biz'
+    },
+    {
+        id: 6,
+        name: 'Antoine Griezmman' ,
+        username: 'Bret', 
+        email: 'Sincere@april.biz'
+    },
+    {
+        id: 7,
+        name: 'Frankie Jong' ,
+        username: 'Bret', 
+        email: 'Sincere@april.biz'
+    },
+    {
+        id: 8,
+        name: 'Karim Benzema' ,
+        username: 'Bret', 
+        email: 'Sincere@april.biz'
+    },
+    {
+        id: 9,
+        name: 'Pep Guardiola' ,
+        username: 'Bret', 
+        email: 'Sincere@april.biz'
+    },
+    {
+        id: 10,
+        name: 'Jurguen Klopp' ,
+        username: 'Bret', 
+        email: 'Sincere@april.biz'
+    },  
+]
